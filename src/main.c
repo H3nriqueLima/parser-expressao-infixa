982ea8cc4d99f33f12218ee78ecee84e9e6ad9ac
H3nriqueLima <henriquelima34121@gmail.com>
2026-08-14 20:59:26 -0300

    feat: abstração da adição de símbolos, operadores e números à lista dinâmica de tokens.

--- src/main.c
+++ src/main.c
@@ -5,10 +5,36 @@
 #include <windows.h>
 #include <token.h>
 
+static bool token_list_add_value_number(TokenList* token_list, int number) {
+	TokenValue t_v;
+	t_v.number_value = number;
+
+	if (!token_list_add(token_list, NUMBER, t_v)) {
+		printf("Erro: falha ao adicionar token (memória).\n");
+		token_list_destroy(&token_list);
+		return false;
+	}
+
+	return true;
+}
+
+static bool token_list_add_value_symbol(TokenList* token_list, ParserTokenType token_type, char symbol) {
+	TokenValue t_v;
+	t_v.symbol_value = symbol;
+
+	if (!token_list_add(token_list, token_type, t_v)) {
+		printf("Erro: falha ao adicionar token (memória).\n");
+		token_list_destroy(&token_list);
+		return false;
+	}
+
+	return true;
+}
+
 int main() {
 	SetConsoleOutputCP(CP_UTF8);
 
-	char test_string[] = "3 + 4 * (22 - 1)";
+	char test_string[] = "3 + 4 * { 7 / 2 + [ 8 - 182 * (22 - 1)]}";
 
 	TokenList* token_list = token_list_create(10);
 	if (token_list == NULL) return 1;
@@ -27,65 +53,30 @@ int main() {
 			}
 
 			int value = atoi(number);
-			TokenValue t_v;
-			t_v.number_value = value;
-
-			if (!token_list_add(token_list, NUMBER, t_v)) {
-				printf("Erro: falha ao adicionar token (memoria).\n");
-				token_list_destroy(&token_list);
-				return 1;
-			}
+			if (!token_list_add_value_number(token_list, value)) return 1;
 			
 			i--;
 
 		} else if (test_string[i] == '+') {
-			TokenValue t_v;
-			t_v.symbol_value = '+';
-			if (!token_list_add(token_list, OPERATOR, t_v)) {
-				printf("Erro: falha ao adicionar token (memoria).\n");
-				token_list_destroy(&token_list);
-				return 1;
-			}
+			if (!token_list_add_value_symbol(token_list, OPERATOR, '+')) return 1;
 		} else if (test_string[i] == '-') {
-			TokenValue t_v;
-			t_v.symbol_value = '-';
-			if (!token_list_add(token_list, OPERATOR, t_v)) {
-				printf("Erro: falha ao adicionar token (memoria).\n");
-				token_list_destroy(&token_list);
-				return 1;
-			}
+			if (!token_list_add_value_symbol(token_list, OPERATOR, '-')) return 1;
 		} else if (test_string[i] == '*') {
-			TokenValue t_v;
-			t_v.symbol_value = '*';
-			if (!token_list_add(token_list, OPERATOR, t_v)) {
-				printf("Erro: falha ao adicionar token (memoria).\n");
-				token_list_destroy(&token_list);
-				return 1;
-			}
+			if (!token_list_add_value_symbol(token_list, OPERATOR, '*')) return 1;
 		} else if (test_string[i] == '/') {
-			TokenValue t_v;
-			t_v.symbol_value = '/';
-			if (!token_list_add(token_list, OPERATOR, t_v)) {
-				printf("Erro: falha ao adicionar token (memoria).\n");
-				token_list_destroy(&token_list);
-				return 1;
-			}
+			if (!token_list_add_value_symbol(token_list, OPERATOR, '/')) return 1;
 		} else if (test_string[i] == '(') {
-			TokenValue t_v;
-			t_v.symbol_value = '(';
-			if (!token_list_add(token_list, GROUPING_SYMBOL, t_v)) {
-				printf("Erro: falha ao adicionar token (memoria).\n");
-				token_list_destroy(&token_list);
-				return 1;
-			}
+			if (!token_list_add_value_symbol(token_list, GROUPING_SYMBOL, '(')) return 1;
 		} else if (test_string[i] == ')') {
-			TokenValue t_v;
-			t_v.symbol_value = ')';
-			if (!token_list_add(token_list, GROUPING_SYMBOL, t_v)) {
-				printf("Erro: falha ao adicionar token (memoria).\n");
-				token_list_destroy(&token_list);
-				return 1;
-			}
+			if (!token_list_add_value_symbol(token_list, GROUPING_SYMBOL, ')')) return 1;
+		} else if (test_string[i] == '[') {
+			if (!token_list_add_value_symbol(token_list, GROUPING_SYMBOL, '[')) return 1;
+		} else if (test_string[i] == ']') {
+			if (!token_list_add_value_symbol(token_list, GROUPING_SYMBOL, ']')) return 1;
+		} else if (test_string[i] == '{') {
+			if (!token_list_add_value_symbol(token_list, GROUPING_SYMBOL, '{')) return 1;
+		} else if (test_string[i] == '}') {
+			if (!token_list_add_value_symbol(token_list, GROUPING_SYMBOL, '}')) return 1;
 		} else if (isspace(test_string[i]) != 0) {
 			// Ignorando espaço.
 		} else {
