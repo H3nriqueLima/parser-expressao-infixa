80820fc105db45eee9e0cdd1100734611abedf2b
H3nriqueLima <henriquelima34121@gmail.com>
2026-08-13 22:55:28 -0300

    feat: integrado tokenizador com TokenList e tratamento de erros de memória.

--- include/token.h
+++ include/token.h
@@ -1,19 +1,23 @@
 #pragma once
 
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 typedef enum {
 	NUMBER,
 	OPERATOR,
 	GROUPING_SYMBOL
-} TokenType;
+} ParserTokenType;
+
+typedef union {
+	int number_value;
+	char symbol_value;
+} TokenValue;
 
 typedef struct {
-	TokenType token_type;
-	union {
-		int number_value;
-		char symbol_value;
-	};
+	ParserTokenType token_type;
+	TokenValue token_value;
 } Token;
 
 typedef struct {
@@ -38,4 +42,63 @@ static inline TokenList* token_list_create(size_t initial_capacity) {
 	}
 
 	return new_token_list;
+}
+
+static inline bool token_list_ensure_capacity(TokenList* token_list) {
+	if (token_list->size < token_list->capacity) return true;
+
+	size_t new_capacity = token_list->capacity * 2;
+
+	Token* new_token_data = (Token*)realloc(token_list->token_data, new_capacity * sizeof(Token));
+	if (new_token_data == NULL) return false;
+
+	token_list->token_data = new_token_data;
+	token_list->capacity = new_capacity;
+
+	return true;
+}
+
+static inline bool token_list_add(TokenList* token_list, ParserTokenType token_type, TokenValue token_value) {
+	if (token_list == NULL) return false;
+	if (!token_list_ensure_capacity(token_list)) return false;
+
+	token_list->token_data[token_list->size].token_type = token_type;
+	token_list->token_data[token_list->size].token_value = token_value;
+
+	token_list->size++;
+
+	return true;
+}
+
+static inline void token_list_destroy(TokenList** token_list) {
+	if (token_list == NULL) {
+		return;
+	} else if (*token_list == NULL) return;
+
+	free((*token_list)->token_data);
+	free(*token_list);
+	
+	*token_list = NULL;
+}
+
+static inline void token_list_print(TokenList* token_list) {
+	if (token_list == NULL) return;
+
+	printf("Lista de Token atual:\n Tamanho da lista: %zu\n Capacidade da lista: %zu\n Dados da lista:\n", token_list->size, token_list->capacity);
+
+	for (size_t i = 0; i < token_list->size; i++) {
+		Token current = token_list->token_data[i];
+
+		switch (current.token_type) {
+			case NUMBER:
+				printf("  [%zu] NUMBER: %d\n", i, current.token_value.number_value);
+				break;
+			case OPERATOR:
+				printf("  [%zu] OPERATOR: %c\n", i, current.token_value.symbol_value);
+				break;
+			case GROUPING_SYMBOL:
+				printf("  [%zu] GROUPING_SYMBOL: %c\n", i, current.token_value.symbol_value);
+				break;
+		}
+	}
 }
--- src/main.c
+++ src/main.c
@@ -3,11 +3,15 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <token.h>
 
 int main() {
 	SetConsoleOutputCP(CP_UTF8);
 
 	char test_string[] = "3 + 4 * (22 - 1)";
+
+	TokenList* token_list = token_list_create(10);
+	if (token_list == NULL) return 1;
 
 	for (size_t i = 0; i < strlen(test_string); i++) {
 
@@ -22,29 +26,82 @@ int main() {
 				j++;
 			}
 
-			printf("Numero: %s\n", number);
+			int value = atoi(number);
+			TokenValue t_v;
+			t_v.number_value = value;
+
+			if (!token_list_add(token_list, NUMBER, t_v)) {
+				printf("Erro: falha ao adicionar token (memoria).\n");
+				token_list_destroy(&token_list);
+				return 1;
+			}
+			
 			i--;
 
 		} else if (test_string[i] == '+') {
-			printf("É soma!\n");
+			TokenValue t_v;
+			t_v.symbol_value = '+';
+			if (!token_list_add(token_list, OPERATOR, t_v)) {
+				printf("Erro: falha ao adicionar token (memoria).\n");
+				token_list_destroy(&token_list);
+				return 1;
+			}
 		} else if (test_string[i] == '-') {
-			printf("É subtração!\n");
+			TokenValue t_v;
+			t_v.symbol_value = '-';
+			if (!token_list_add(token_list, OPERATOR, t_v)) {
+				printf("Erro: falha ao adicionar token (memoria).\n");
+				token_list_destroy(&token_list);
+				return 1;
+			}
 		} else if (test_string[i] == '*') {
-			printf("É multiplicação!\n");
+			TokenValue t_v;
+			t_v.symbol_value = '*';
+			if (!token_list_add(token_list, OPERATOR, t_v)) {
+				printf("Erro: falha ao adicionar token (memoria).\n");
+				token_list_destroy(&token_list);
+				return 1;
+			}
 		} else if (test_string[i] == '/') {
-			printf("É divisão!\n");
+			TokenValue t_v;
+			t_v.symbol_value = '/';
+			if (!token_list_add(token_list, OPERATOR, t_v)) {
+				printf("Erro: falha ao adicionar token (memoria).\n");
+				token_list_destroy(&token_list);
+				return 1;
+			}
 		} else if (test_string[i] == '(') {
-			printf("É parêntese-abre!\n");
+			TokenValue t_v;
+			t_v.symbol_value = '(';
+			if (!token_list_add(token_list, GROUPING_SYMBOL, t_v)) {
+				printf("Erro: falha ao adicionar token (memoria).\n");
+				token_list_destroy(&token_list);
+				return 1;
+			}
 		} else if (test_string[i] == ')') {
-			printf("É parêntese-fecha!\n");
+			TokenValue t_v;
+			t_v.symbol_value = ')';
+			if (!token_list_add(token_list, GROUPING_SYMBOL, t_v)) {
+				printf("Erro: falha ao adicionar token (memoria).\n");
+				token_list_destroy(&token_list);
+				return 1;
+			}
 		} else if (isspace(test_string[i]) != 0) {
 			// Ignorando espaço.
 		} else {
 			printf("Erro: caractere inválido '%c'\n", test_string[i]);
+			token_list_destroy(&token_list);
+
 			return 1;
 		}
 
 	}
+
+	token_list_print(token_list);
+
+	system("pause");
+
+	token_list_destroy(&token_list);
 
 	return 0;
 }
